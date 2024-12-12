68f3efb7736dda3790c592841cc19947db483b60
Jasareno-Christian-M-BSCS21A1 <christianjasareno80@gmail.com>
2024-12-12 17:37:33 +0800

    dashboard

--- Muntinlupa Bus E-Wallet/Route&FareManagementForm.Designer.cs
+++ Muntinlupa Bus E-Wallet/Route&FareManagementForm.Designer.cs
@@ -32,10 +32,11 @@
             // 
             // Route_FareManagementForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(835, 432);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Route_FareManagementForm";
             this.Text = "Route_FareManagementForm";
             this.ResumeLayout(false);
---
+++ Muntinlupa Bus E-Wallet/dashboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Muntinlupa_Bus_E_Wallet
+{
+    public partial class frmAdminDashboard : Form
+    {
+        public frmAdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
