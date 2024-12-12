6b8e6db2a3dfd1ebca552810e3871b253411c696
nyxiee10 <blessydel@gmail.com>
2024-12-12 19:32:00 +0800

    Route and Fare Management Form
    - Route and Fare Management UI

--- Muntinlupa Bus E-Wallet/Route&FareManagementForm.Designer.cs
+++ Muntinlupa Bus E-Wallet/Route&FareManagementForm.Designer.cs
@@ -29,27 +29,28 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Route_FareManagementForm));
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cmbPickupPoint = new System.Windows.Forms.ComboBox();
+            this.cmbDropOffPoint = new System.Windows.Forms.ComboBox();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveRoute = new System.Windows.Forms.Button();
+            this.lblFare = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cmbPickupPoint
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(254, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cmbPickupPoint.FormattingEnabled = true;
+            this.cmbPickupPoint.Location = new System.Drawing.Point(282, 164);
+            this.cmbPickupPoint.Name = "cmbPickupPoint";
+            this.cmbPickupPoint.Size = new System.Drawing.Size(254, 21);
+            this.cmbPickupPoint.TabIndex = 0;
             // 
-            // comboBox2
+            // cmbDropOffPoint
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(12, 39);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(254, 21);
-            this.comboBox2.TabIndex = 1;
+            this.cmbDropOffPoint.FormattingEnabled = true;
+            this.cmbDropOffPoint.Location = new System.Drawing.Point(282, 220);
+            this.cmbDropOffPoint.Name = "cmbDropOffPoint";
+            this.cmbDropOffPoint.Size = new System.Drawing.Size(254, 21);
+            this.cmbDropOffPoint.TabIndex = 1;
             // 
             // btnCancel
             // 
@@ -59,48 +60,64 @@
             this.btnCancel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
             this.btnCancel.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.btnCancel.Image = ((System.Drawing.Image)(resources.GetObject("btnCancel.Image")));
-            this.btnCancel.Location = new System.Drawing.Point(178, 200);
+            this.btnCancel.Location = new System.Drawing.Point(515, 309);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(98, 31);
             this.btnCancel.TabIndex = 8;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = false;
             // 
-            // btnSave
+            // btnSaveRoute
             // 
-            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnSave.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
-            this.btnSave.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
-            this.btnSave.Location = new System.Drawing.Point(17, 200);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(155, 31);
-            this.btnSave.TabIndex = 7;
-            this.btnSave.Text = "Save Account";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSaveRoute.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnSaveRoute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSaveRoute.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSaveRoute.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSaveRoute.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveRoute.Image")));
+            this.btnSaveRoute.Location = new System.Drawing.Point(354, 309);
+            this.btnSaveRoute.Name = "btnSaveRoute";
+            this.btnSaveRoute.Size = new System.Drawing.Size(155, 31);
+            this.btnSaveRoute.TabIndex = 7;
+            this.btnSaveRoute.Text = "Save Route Details";
+            this.btnSaveRoute.UseVisualStyleBackColor = false;
+            // 
+            // lblFare
+            // 
+            this.lblFare.AutoSize = true;
+            this.lblFare.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFare.Location = new System.Drawing.Point(278, 267);
+            this.lblFare.Name = "lblFare";
+            this.lblFare.Size = new System.Drawing.Size(46, 21);
+            this.lblFare.TabIndex = 9;
+            this.lblFare.Text = "Fare";
             // 
             // Route_FareManagementForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(626, 351);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(835, 432);
+            this.Controls.Add(this.lblFare);
             this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnSaveRoute);
+            this.Controls.Add(this.cmbDropOffPoint);
+            this.Controls.Add(this.cmbPickupPoint);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Route_FareManagementForm";
             this.Text = "Route_FareManagementForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cmbPickupPoint;
+        private System.Windows.Forms.ComboBox cmbDropOffPoint;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveRoute;
+        private System.Windows.Forms.Label lblFare;
     }
 }
