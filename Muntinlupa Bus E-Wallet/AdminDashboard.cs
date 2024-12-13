6016ad0cc71b7fb2717320f7bb73720b2f5eb675
nyxiee10 <blessydel@gmail.com>
2024-12-13 09:12:11 +0800

    Admin Dashboard
    - Admin Dashboard UI

---
+++ Muntinlupa Bus E-Wallet/AdminDashboard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Muntinlupa_Bus_E_Wallet
+{
+    public partial class frmAdminDashboard : Form
+    {
+        public frmAdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void lblAccountManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmAdminDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCardExpirationHistory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblReloadAccount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
