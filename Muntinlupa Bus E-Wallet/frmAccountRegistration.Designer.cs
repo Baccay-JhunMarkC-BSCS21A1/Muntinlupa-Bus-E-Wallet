0b9dee541f874702aa326ea35cf82535e65f6ea4
Baccay-JhunMarkC-BSCS21A1 <C1694-23@itmlyceumalabang.onmicrosoft.com>
2024-12-14 03:11:53 +0800

    Account Registration

---
+++ Muntinlupa Bus E-Wallet/frmAccountRegistration.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Muntinlupa_Bus_E_Wallet
+{
+    partial class frmAccountRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtContactInfo = new System.Windows.Forms.TextBox();
+            this.txtInitialBalance = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dtpExpirationDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contact Information:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Balance:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Expiration Date:";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(358, 90);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerName.TabIndex = 4;
+            // 
+            // txtContactInfo
+            // 
+            this.txtContactInfo.Location = new System.Drawing.Point(358, 128);
+            this.txtContactInfo.Name = "txtContactInfo";
+            this.txtContactInfo.Size = new System.Drawing.Size(100, 20);
+            this.txtContactInfo.TabIndex = 5;
+            // 
+            // txtInitialBalance
+            // 
+            this.txtInitialBalance.Location = new System.Drawing.Point(358, 165);
+            this.txtInitialBalance.Name = "txtInitialBalance";
+            this.txtInitialBalance.Size = new System.Drawing.Size(100, 20);
+            this.txtInitialBalance.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(358, 269);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(466, 269);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click_1);
+            // 
+            // dtpExpirationDate
+            // 
+            this.dtpExpirationDate.Location = new System.Drawing.Point(358, 203);
+            this.dtpExpirationDate.Name = "dtpExpirationDate";
+            this.dtpExpirationDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpExpirationDate.TabIndex = 9;
+            // 
+            // frmAccountRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 432);
+            this.Controls.Add(this.dtpExpirationDate);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtInitialBalance);
+            this.Controls.Add(this.txtContactInfo);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmAccountRegistration";
+            this.Text = "frmAccountRegistration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtContactInfo;
+        private System.Windows.Forms.TextBox txtInitialBalance;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DateTimePicker dtpExpirationDate;
+    }
+}
---
+++ Muntinlupa Bus E-Wallet/frmAccountRegistration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Muntinlupa_Bus_E_Wallet
+{
+    public partial class frmAccountRegistration : Form
+    {
+        public frmAccountRegistration()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void ClearFields()
+        {
+            txtCustomerName.Clear();
+            txtContactInfo.Clear();
+            txtInitialBalance.Clear();
+            dtpExpirationDate.Value = DateTime.Now;
+        }
+
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCustomerName.Text) || string.IsNullOrWhiteSpace(txtContactInfo.Text) ||
+                string.IsNullOrWhiteSpace(txtInitialBalance.Text) || !decimal.TryParse(txtInitialBalance.Text, out decimal initialBalance))
+            {
+                MessageBox.Show("Please fill in all fields correctly.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DateTime expirationDate = dtpExpirationDate.Value;
+
+            CustomerAccount newAccount = new CustomerAccount(txtCustomerName.Text, txtContactInfo.Text, initialBalance, expirationDate);
+
+            bool isSaved = newAccount.SaveAccount();
+
+            if (isSaved)
+            {
+                MessageBox.Show("Account successfully registered!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearFields();
+            }
+            else
+            {
+                MessageBox.Show("Failed to register the account.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click_1(object sender, EventArgs e)
+        {
+            frmAdminDashboard form = new frmAdminDashboard();
+            form.Show();
+            ClearFields();
+            this.Close();
+        }
+    }
+}
